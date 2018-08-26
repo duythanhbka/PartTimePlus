837dfbc3b9ff96eb6308203dd28d04d0a0704d00
ndt <duythanh.bachkhoahanoi@gmail.com>
2018-08-26 14:54:31 +0700

    Done: P1 and P2: a, b

---
+++ Plugin/CheckCartPlugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ndt
+ * Date: 26/08/2018
+ * Time: 13:49
+ */
+namespace Magenest\PartTimePlus2\Plugin;
+
+class CheckCartPlugin
+{
+    public function afterHasItems(\Magento\Quote\Model\Quote $subject, $result)
+    {
+        $items = $subject->getData('items');
+        foreach ($items as $item) {
+            $quantity = $item->getData('qty');
+            if ($quantity > 1) {
+                return false;
+            }
+        }
+        return $result;
+    }
+}
